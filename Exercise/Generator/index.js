a3b86054bd7f05b741a4bda7d910dbc5169ee44c
Huang Jian <huang.jian@howtimeflies.io>
2022-01-23 21:47:19 +0800

    制作数学计算题。

--- Exercise/Generator/index.js
+++ Exercise/Generator/index.js
@@ -26,12 +26,51 @@ function closeErrorMessage() {
   $('.error').classList.add('hidden')
 }
 
+function make() {
+  makeChars()
+  makeMaths()
+}
+
+$('#calculations').value = `
+1+1=?,1+2=?,1+3=?,1+4=?
+2+2=?,2+4=?,2+6=?,2+8=?
+3+3=?,3+4=?,3+5=?,3+6=?
+5-4=?,5-3=?,5-2=?,5-1=?
+4-4=?,4-3=?,4-2=?,4-1=?
+---
+8+3=?,2+9=?,7+4=?,6+6=?
+6+?=9,?+2=10,7-?=3,?-5=5
+---
+3+4+2=?,5+4-6=?
+`.trim()
+
+function makeMaths() {
+  const lines = ($('#calculations').value || '').split('\n')
+
+  const html = lines.map(line => {
+    if (line.startsWith('---')) {
+      return '<hr class="mt-4">'
+    }
+    
+    const items = line.split(',')
+    const inner = items.map(item => {
+      const question = item.replaceAll(/\?/g, `<span class="box"></span>`)
+      return `<div class="flex-1">${question}</div>`
+    }).join('\n')
+    return `<div class="flex origin-left mt-4">${inner}</div>`
+  }).join('\n')
+
+  $('#math').innerHTML = html
+}
+
 const charSize = 42
 /**
  * 生成练习题。
  */
-function make() {
-  const char = $('#char-to-practise').value || '请输入汉字'
+function makeChars() {
+  const char = $('#char-to-practise').value
+  if (!char) return
+
   // https://theajack.gitee.io/cnchar/doc/draw.html#_2-%E4%BD%BF%E7%94%A8
   cnchar.draw(char, {        
     el: '#char',        
