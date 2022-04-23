ba671f63a20f2579dfd933bf47f726736717e2d6
ustc.hj@gmail.com <ustc.hj@gmail.com>
2022-04-23 11:47:11 +0800

    过滤生僻字的拆字数据

--- Generator/gist/convert_chaizi_txt_to_json.js
+++ Generator/gist/convert_chaizi_txt_to_json.js
@@ -9,13 +9,29 @@ fs.readFile('../data/chaizi-jt.txt', 'utf8', (err, data) => {
     convertDataToJson(data)
 })
 
+// https://stackoverflow.com/a/43419070/474231
+// U+3040 - U+30FF: hiragana and katakana (Japanese only)
+// U+3400 - U+4DBF: CJK unified ideographs extension A (Chinese, Japanese, and Korean)
+// U+4E00 - U+9FFF: CJK unified ideographs (Chinese, Japanese, and Korean)
+// U+F900 - U+FAFF: CJK compatibility ideographs (Chinese, Japanese, and Korean)
+// U+FF66 - U+FF9F: half-width katakana (Japanese only)
+
+const cjkPattern =/[\u4e00-\u9fff]/
+const invisibleChars = []
 function convertDataToJson(data) {
     const result = {}
     data.split('\n').forEach(row => {
         const arr = row.split('\t')
         const id = arr[0]
-        result[id] = arr.filter((_, idx) => idx > 0).map(it => it.split(' '))
+        if (!cjkPattern.test(id)) {
+            invisibleChars.push(id)
+        } else {
+            result[id] = arr.filter((_, idx) => idx > 0).map(it => it.split(' '))
+        }
     })
+
+    // console.log(invisibleChars)
+    // console.log(invisibleChars.length)
 
     //   console.log(result)
     fs.writeFile('../data/chaizi.json', JSON.stringify(result), err => console.log(`err = ${err}`))
