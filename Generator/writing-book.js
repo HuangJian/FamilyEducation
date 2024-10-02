7f58be7367c91dc411ea0153a8e6333b213e3d50
ustc.hj@gmail.com <ustc.hj@gmail.com>
2024-10-02 16:24:59 +0800

    汉字字帖：支持打印汉语拼音。

--- Generator/writing-book.js
+++ Generator/writing-book.js
@@ -1,31 +1,30 @@
 const sampleRow = document.querySelector('.row').cloneNode(true)
 sampleRow.classList.remove('hidden')
-const sampleBox = sampleRow.querySelector('.box')
+const sampleBox = document.querySelector('.row .box')
+const samplePinyinBox = document.querySelector('.row .pinyin')
 
 document.querySelector('#words').value =
-    '2 百战百胜 割麦 打谷 积肥 锄草 勇往直前 紫色 笑眯眯 狐狸 笨蛋 酸菜 己 所 不 欲 勿 施 于 人'
+  '2 chuǎng闯荡世界 gē+mài+割麦 打谷 积肥 锄草 勇往直前 笑眯眯 狐狸 笨蛋 酸菜 己 所 不 欲 勿 施 于 人'
 
 const rowsPerSheet = 16
 const maxColumns = 8
 const emptyRow = sampleRow.cloneNode(false)
 Array.from({ length: maxColumns }, () => {
-    emptyRow.appendChild(sampleBox.cloneNode(true))
+  emptyRow.appendChild(sampleBox.cloneNode(true))
 })
 
 make()
 
 function make() {
-    document.querySelector('#printable').innerHTML = ''
-    document.querySelector('#screen').innerHTML = ''
+  document.querySelector('#printable').innerHTML = ''
+  document.querySelector('#screen').innerHTML = ''
 
-    document.querySelector('#words').value.trim()
-        .split(/\s+/)
-        .forEach(printWord)
-    layoutScreen()
+  document.querySelector('#words').value.trim().split(/\s+/).forEach(printWord)
+  layoutScreen()
 }
 
 function doPrint() {
-    window.print()
+  window.print()
 }
 
 /**
@@ -34,91 +33,117 @@ function doPrint() {
  * @return {Element}
  */
 function htmlToElement(html) {
-    var template = document.createElement('template')
-    template.innerHTML = html.trim() // Never return a text node of whitespace as the result
-    return template.content.firstChild
+  var template = document.createElement('template')
+  template.innerHTML = html.trim() // Never return a text node of whitespace as the result
+  return template.content.firstChild
 }
 
 function printWord(text) {
-    const row = sampleRow.cloneNode(false)
+  const row = sampleRow.cloneNode(false)
 
-    // 纯数字，则增加几个空白行。便于复用打印纸。
-    if (/^\d+$/.test(text)) {
-        const blankRow = sampleRow.cloneNode(true)
-        blankRow.classList.add('opacity-0')
-        Array.from({ length: parseInt(text) }, () => addRow(blankRow.cloneNode(true)))
+  // 纯数字，则增加几个空白行。便于复用打印纸。
+  if (/^\d+$/.test(text)) {
+    printBlankRows(parseInt(text))
+    return
+  }
 
-        return
-    }
+  let word = text
+  const cjkPattern = /[\u4e00-\u9fff]/g
+  if (!cjkPattern.test(text[0])) {
+    const pinyin = text.replace(cjkPattern, '')
+    word = text.replace(pinyin, '')
+    printPinyinRow(pinyin)
+  }
 
-    const toAddMoreEmptyRows = /\+\d+$/.test(text)
-    // 四行+2 => 四行
-    const word = toAddMoreEmptyRows ? /^(.+?)\+\d+$/.exec(text)[1] : text
+  const toAddMoreEmptyRows = /\+\d+$/.test(word)
+  // 四行+2 => 四行
+  word = toAddMoreEmptyRows ? /^(.+?)\+\d+$/.exec(word)[1] : word
 
-    const settings = [
-        {size: 1, sample: 3, split: 0, extraRows: 0},
-        {size: 2, sample: 2, split: 1, extraRows: 1},
-        {size: 3, sample: 1, split: 1, extraRows: 1},
-        {size: 4, sample: 1, split: 0, extraRows: 2},
-    ]
-    const setting = settings[word.length % 5 - 1]
-    let extraRows = toAddMoreEmptyRows
-        // 四行+2 => 2
-        ? parseInt(/\+(\d+)$/.exec(text)[1])  + setting.extraRows
-        : setting.extraRows
+  const settings = [
+    { size: 1, sample: 3, split: 0, extraRows: 0 },
+    { size: 2, sample: 2, split: 1, extraRows: 1 },
+    { size: 3, sample: 1, split: 1, extraRows: 1 },
+    { size: 4, sample: 1, split: 0, extraRows: 1 },
+  ]
+  const setting = settings[(word.length % 5) - 1]
+  let extraRows = toAddMoreEmptyRows
+    ? // 四行+2 => 2
+      parseInt(/\+(\d+)$/.exec(text)[1]) + setting.extraRows
+    : setting.extraRows
 
-    const chars = word.split('')
-    printChars(chars, row)
+  const chars = word.split('')
+  printChars(chars, row)
 
-    Array.from({ length: setting.sample }, () => {
-        Array.from({ length: setting.split }, () => {
-            row.appendChild(sampleBox.cloneNode(true))
-        })
-
-        printChars(chars, row, 'sample')
+  Array.from({ length: setting.sample }, () => {
+    Array.from({ length: setting.split }, () => {
+      row.appendChild(sampleBox.cloneNode(true))
     })
-    Array.from({ length: maxColumns - row.childNodes.length }, () => {
-        row.appendChild(sampleBox.cloneNode(true))
-    })
-    addRow(row)
 
-    Array.from({ length: extraRows }, () => addRow(emptyRow.cloneNode(true)))
+    printChars(chars, row, 'sample')
+  })
+  Array.from({ length: maxColumns - row.childNodes.length }, () => {
+    row.appendChild(sampleBox.cloneNode(true))
+  })
+  addRow(row)
+
+  Array.from({ length: extraRows }, () => addRow(emptyRow.cloneNode(true)))
 }
 
 function printChars(chars, row, extraClass) {
-    chars.forEach(it => {
-        const box = sampleBox.cloneNode(true)
-        const char = box.querySelector('.char')
-        char.innerHTML = it
-        if (extraClass) {
-            char.classList.add(extraClass)
-        }
+  chars.forEach((it) => {
+    const box = sampleBox.cloneNode(true)
+    const char = box.querySelector('.char')
+    char.innerHTML = it
+    if (extraClass) {
+      char.classList.add(extraClass)
+    }
 
-        row.appendChild(box)
-    })
+    row.appendChild(box)
+  })
+}
+
+function printBlankRows(rows) {
+  const blankRow = sampleRow.cloneNode(true)
+  blankRow.classList.add('opacity-0')
+  Array.from({ length: rows }, () => addRow(blankRow.cloneNode(true)))
+}
+
+function printPinyinRow(pinyin) {
+  const row = sampleRow.cloneNode(false)
+  const box = samplePinyinBox.cloneNode(true)
+  const arr = pinyin.split('+')
+  arr.forEach((item) => {
+    const box = samplePinyinBox.cloneNode(true)
+    box.querySelector('span').innerText = item
+    row.appendChild(box)
+  })
+  Array.from({ length: maxColumns - arr.length }, () =>
+    row.appendChild(samplePinyinBox.cloneNode(true))
+  )
+  addRow(row)
 }
 
 function addRow(row) {
-    document.querySelector('#printable').appendChild(row)
+  document.querySelector('#printable').appendChild(row)
 }
 
 function layoutScreen() {
-    const screen = document.querySelector('#screen')
+  const screen = document.querySelector('#screen')
 
-    const sheetHeight = screen.offsetHeight * .9
-    const boxSize = (sheetHeight + .0) / rowsPerSheet - 2
-    document.documentElement.style.setProperty('--box-size', `${boxSize}px`);
+  const sheetHeight = screen.offsetHeight * 0.9
+  const boxSize = (sheetHeight + 0.0) / rowsPerSheet - 2
+  document.documentElement.style.setProperty('--box-size', `${boxSize}px`)
 
-    let sheet
-    const sheetHtml = `<div class="sheet w-1/2 flex-col mx-auto"></div>`
+  let sheet
+  const sheetHtml = `<div class="sheet w-1/2 flex-col mx-auto"></div>`
 
-    screen.innerHTML = ''
-    document.querySelectorAll('#printable .row').forEach((row, idx) => {
-        if (idx % rowsPerSheet === 0) {
-            sheet = htmlToElement(sheetHtml)
-            screen.appendChild(sheet)
-        }
+  screen.innerHTML = ''
+  document.querySelectorAll('#printable .row').forEach((row, idx) => {
+    if (idx % rowsPerSheet === 0) {
+      sheet = htmlToElement(sheetHtml)
+      screen.appendChild(sheet)
+    }
 
-        sheet.appendChild(row.cloneNode(true))
-    })
+    sheet.appendChild(row.cloneNode(true))
+  })
 }
